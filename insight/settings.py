29a18a874cf42d6dc9cdb6a67fc53e35c97403ae
SeanTangney <sean_tangney@hotmail.com>
2022-05-05 10:48:37 +0000

    Migrate to heroku database

--- insight/settings.py
+++ insight/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import osimport dj_database_url
+import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -83,6 +84,10 @@ WSGI_APPLICATION = 'insight.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
