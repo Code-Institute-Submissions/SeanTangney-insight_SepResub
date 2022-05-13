fd875ae9f9bafa1f94510e06202d737cc3647c78
SeanTangney <sean_tangney@hotmail.com>
2022-05-13 13:51:23 +0000

    Change DATABASE_URL in env.py after heroku database maintenance

--- newssite/models.py
+++ newssite/models.py
@@ -4,17 +4,21 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name ="news_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news_posts"
+        )
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='news_likes', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='news_likes', blank=True)
 
     class Meta:
         ordering = ['-created_on']
@@ -27,13 +31,14 @@ class Post(models.Model):
 
 
 class Comments(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+        )
     name = models.CharField(max_length=100)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
-
 
     class Meta:
         ordering = ['-created_on']
