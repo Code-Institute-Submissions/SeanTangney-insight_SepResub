882678c16e54741d21378fffb64a8f03d855a4fa
SeanTangney <sean_tangney@hotmail.com>
2022-05-13 15:50:51 +0000

    Import admin

--- insight/settings.py
+++ insight/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['insight2022.herokuapp.com', 'locahost']
+ALLOWED_HOSTS = ["insight2022.herokuapp.com", "localhost"]
 
 
 # Application definition
