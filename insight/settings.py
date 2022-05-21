fc8c4efb645738705ba0e10c5135075d402747f9
SeanTangney <sean_tangney@hotmail.com>
2022-05-21 19:49:09 +0000

    Install allauth and add to urls.py and settings.py

--- insight/settings.py
+++ insight/settings.py
@@ -41,12 +41,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'newssite',
 ]
+
+SITE_ID = 1
+
+# Redirects the user to the homepage after logging in/out
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
