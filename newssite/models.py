a7257db7fbd106a81829838855f256d9906edf26
SeanTangney <sean_tangney@hotmail.com>
2022-06-03 18:07:38 +0000

    Assign max-length to body in post model

--- newssite/models.py
+++ newssite/models.py
@@ -7,14 +7,14 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="news_posts"
     )
     updated_on = models.DateTimeField(auto_now=True)
     body = RichTextField(blank=True, null=True)
-    body = models.TextField()
+    body = models.TextField(max_length=200)
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -37,7 +37,7 @@ class Comment(models.Model):
         Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    body = models.TextField()
+    body = models.TextField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=True)
 
